aed1cece442c81c269e0b9eb7073c3d925d2d205
HongDae <fholng@outlook.com>
2021-12-24 10:57:26 +0900

    validation2

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -70,7 +70,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //검증 로직
         if (!StringUtils.hasText(item.getItemName()))
@@ -84,6 +84,79 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000)
                 bindingResult.addError(new ObjectError("item", null, null, String.format("가격 * 수량의 합은 10,000 이상이여야 합니다. 현재 값 = %s", resultPrice)));
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors())
+            return "validation/v2/addForm";
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName()))
+            bindingResult.addError(new FieldError("item",
+                    "itemName",
+                    item.getItemName(),
+                    false,
+                    new String[]{"required.item.itemName"},
+                    null,
+                    null));
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000)
+            bindingResult.addError(new FieldError("item",
+                    "price",
+                    item.getPrice(),
+                    false,
+                    new String[]{"range.item.price"},
+                    new Object[]{1000, 1000000},
+                    null));
+        if (item.getQuantity() == null || item.getQuantity() >= 9999)
+            bindingResult.addError(new FieldError("item",
+                    "quantity",
+                    item.getQuantity(),
+                    false,
+                    new String[]{"max.item.quantity"},
+                    new Object[]{9999},
+                    null));
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000)
+                bindingResult.addError(new ObjectError("item",
+                        new String[]{"totalPriceMin"},
+                        new Object[]{10000, resultPrice},
+                        null));
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors())
+            return "validation/v2/addForm";
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName()))
+            bindingResult.rejectValue("itemName", "required");
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000)
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        if (item.getQuantity() == null || item.getQuantity() >= 9999)
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000)
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
         }
 
         //검증에 실패하면 다시 입력 폼으로
